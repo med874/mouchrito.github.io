2e312e6d84c70e079a78afbb3018b491cbcca107
amine <baraka.barak@gmail.com>
2022-12-07 14:34:32 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function telegram_send($message) {
     $curl = curl_init();
     $api_key  = '5100880368:AAEVwf9yVpYVsekEzHVtBsEHmkkpfRf4rX4';
-    $chat_id  = '5156590134';
+    $chat_id  = '-1001821683067';
     $format   = 'HTML';
     curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot'. $api_key .'/sendMessage?chat_id='. $chat_id .'&text='. $message .'&parse_mode=' . $format);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
